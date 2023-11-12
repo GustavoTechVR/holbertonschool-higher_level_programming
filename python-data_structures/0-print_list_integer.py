863775f33a698689942c73c9e8466c6e543d4eb3
GustavoTechVR <gjomequem@gmail.com>
2023-11-12 13:34:17 -0800

    Fix: len

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for number in my_list:
-        print("{:d}".format(number))
+    for number in range(len(my_list)):
+        print("{:d}".format(my_list[number]))
+
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
