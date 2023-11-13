fd51fd1f53d9ad519da059b2e36376f29c4af270
GustavoTechVR <gjomequem@gmail.com>
2023-11-12 16:40:20 -0800

    Fix: join

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str[39:67] + str[-22:-17] + str[0:6]
+         language that combines remarkable power with very clear syntax"
+str = ' '.join(str.split()[5:7] + [str.split()[-4]] + [str.split()[0]])
 print(str)
