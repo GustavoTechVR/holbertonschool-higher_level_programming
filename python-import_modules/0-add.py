a27fe79d0cf00319fbac9bbd5577d0a3860781a7
GustavoTechVR <gjomequem@gmail.com>
2023-11-12 21:03:42 -0800

    Fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,6 +4,7 @@ a = 1
 b = 2
 
 if __name__ == "__main__":
-    add_0 = __import__('add_0')
-    resultado = add_0.add(a, b)
+    add_0 = {}
+    exec(open('add_0.py').read(), add_0)
+    resultado = add_0['add'](a, b)
     print("{} + {} = {}".format(a, b, resultado))
