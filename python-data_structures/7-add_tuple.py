384043f48d1bc6c3d9bbec783d2b15424e55836b
GustavoTechVR <gjomequem@gmail.com>
2023-11-12 19:09:45 -0800

    Fix: blacklines

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,6 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result_tuple
 
+
 if __name__ == "__main__":
     tuple_a = (1, 89)
     tuple_b = (88, 11)
