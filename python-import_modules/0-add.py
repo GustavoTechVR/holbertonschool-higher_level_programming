3648162a446ec62e55cbb1843c9abf0a83756962
GustavoTechVR <gjomequem@gmail.com>
2023-11-12 20:44:33 -0800

    Fix: character v2

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     a = 1
     b = 2
     exec(open("add_0.py").read())
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
