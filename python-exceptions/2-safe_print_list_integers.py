1bd922158dd0f1838804f01ebe4482d94482a997
GustavoTechVR <gjomequem@gmail.com>
2023-11-21 19:44:23 -0800

    Fix: traceback

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 printed_integers += 1
     except (IndexError, ValueError, TypeError):
-        pass
+        print("\n{}".format(e))
 
     print()
     return printed_integers
