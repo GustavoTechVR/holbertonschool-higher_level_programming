d59777d7130a5cf18d98739a6b8d1e502f02574b
GustavoTechVR <gjomequem@gmail.com>
2023-11-22 09:27:28 -0800

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """Empty class Square.
-    Defines a square with no attributes or methods.
-
     """
-
+    Empty class Square.
+    Defines a square with no attributes or methods.
+    """
     pass
