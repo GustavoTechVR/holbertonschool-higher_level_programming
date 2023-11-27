bad0d3c6f0ca29699a335d36c07b0f57e11bbc58
GustavoTechVR <gjomequem@gmail.com>
2023-11-26 19:14:54 -0800

    4. Divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result_list = []
+    """
+    Divides element by element of two lists and returns a new list with all divisions.
+    """
+    result = []
 
     for i in range(list_length):
         try:
-            element_1 = my_list_1[i] if i < len(my_list_1) else None
-            element_2 = my_list_2[i] if i < len(my_list_2) else None
+            division = my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError, TypeError):
+            if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                print("division by 0")
+                division = 0
+            elif not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+                print("wrong type")
+                division = 0
+            else:
+                print("out of range")
+                division = 0
+        finally:
+            result.append(division)
 
-            if element_1 is None or element_2 is None:
-                raise IndexError("out of range")
-
-            result = element_1 / element_2
-            result_list.append(result)
-
-        except ZeroDivisionError:
-            print("division by 0")
-            result_list.append(0)
-
-        except (TypeError, ValueError):
-            print("wrong type")
-            result_list.append(0)
-
-        except IndexError:
-            print("out of range")
-            result_list.append(0)
-
-    return result_list
+    return result
