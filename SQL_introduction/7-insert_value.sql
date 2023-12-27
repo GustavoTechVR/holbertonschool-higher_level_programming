dbdd5bab9bd2c9f14a87ad0a9b327f3fe10dc0d5
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 13:10:25 -0800

    Feat: added 7-insert_value.sql

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- File: 7-insert_value.sql
+-- Description: This script inserts a new row into the table first_table in the specified database.
+
+-- Insert a new row
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
