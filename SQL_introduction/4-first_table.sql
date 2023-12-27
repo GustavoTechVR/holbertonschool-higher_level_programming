6b07c530ea52802835231172f19f9b621e25e9dc
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 12:43:03 -0800

    Feat: added 4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- File: 4-first_table.sql
+-- Description: This script creates the table first_table in the specified database.
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
