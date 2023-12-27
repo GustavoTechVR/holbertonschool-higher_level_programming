2ddac0dbc65168851aee852636690db81ebe202a
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 12:46:45 -0800

    Feat: added 5-full_table.sql

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- File: 5-full_table.sql
+-- Description: This script prints the full description of the table first_table in the specified database.
+
+-- Print the full description of the table
+SHOW CREATE TABLE first_table;
