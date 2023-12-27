3c5a1c0ca2e472ef4a0d7c0cb50d53ff9dcc795f
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 12:30:18 -0800

    Feat: added 1-create_database_if_missing.sql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- File: 1-create_database_if_missing.sql
+-- Description: This script creates the database hbtn_0c_0 if it doesn't already exist.
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
