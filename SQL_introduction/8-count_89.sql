47963762f7e34cff64fd6a1140ffc585f2a7f466
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 13:11:47 -0800

    Feat: added 8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- File: 8-count_89.sql
+-- Description: This script displays the number of records with id = 89 in the table first_table of the specified database.
+
+-- Select the count of records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
