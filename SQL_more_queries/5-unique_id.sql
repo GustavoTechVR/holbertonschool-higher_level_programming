23a6ccf48b19006f2e27e4050063bad3957c090f
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 13:57:14 -0800

    Feat: added 5-unique_id.sql

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,8 @@
+-- Task: Create table unique_id
+-- File: 5-unique_id.sql
+
+-- Create table unique_id if not exists
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
