47ffd8809580057e22ec9587d59b36e06787fb21
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 13:21:57 -0800

    Feat: added 14-average.sql

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- File: 14-average.sql
+-- Description: This script computes the average score of all records in the table second_table
+-- in the specified database.
+
+-- Compute the average score
+SELECT AVG(score) AS average FROM second_table;
