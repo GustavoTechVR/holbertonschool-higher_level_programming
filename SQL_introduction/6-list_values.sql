4782e2f3fc383c1f64f67ca640df2b9aadfcd887
GustavoTechVR <gjomequem@gmail.com>
2023-12-27 12:55:34 -0800

    Feat: added 6-list_value.sql

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- File: 6-list_values.sql
+-- Description: This script lists all rows of the table first_table in the specified database.
+
+-- Select all rows from the table
+SELECT * FROM first_table;
